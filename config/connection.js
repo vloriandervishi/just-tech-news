989b817498940cd60309f9a77a314a312790f2ab
Valdo <vlorian22@GMAIL.COM>
2021-01-11 13:40:16 -0600

    setup the project

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize");
+const sequelize=require("sequelize");
+
+const sequelize= new Sequelize('just_tech_news_db','username','password',{
+ host:'localhost',
+ dialect:'mysql',
+ port: 3306
+});
+
+module.exports=sequelize;
