9bee9361bef3f116617dcaaa5d94039cb43d2a53
Valdo <vlorian22@GMAIL.COM>
2021-01-12 16:20:07 -0600

    finished 13.2 module

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,9 +49,29 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  router.post('/login',(req,res) =>{
+     User.findOne({
+       where:{
+         email:req.body.email
+       }
+     }).then(dbUserData=>{
+         if(!dbUserData){
+           res.status(400).json({message:'No user with that email address!'});
+           return;
+         }
+         const validPassword=dbUserData.checkPassword(req.body.password);
+         if(!validPassword){
+           res.status(400).json({message:'Incorrect password!'});
+           return;
+         }
+         // add comment syntax in front of this line in the .then()
+         res.json({user:dbUserData,message: 'You are now logged in!'});
+     });
+  });
 // put /api/users/1
 router.put('/:id',(req,res)=>{
     User.update(req.body,{
+      individualHooks:true,
         where:{
             id:req.params.id
         }
